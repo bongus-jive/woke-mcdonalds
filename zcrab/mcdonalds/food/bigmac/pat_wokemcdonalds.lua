973c57154ca50befb531ee31b35c7ef361f1f8d2
patman <27874300+patmanf@users.noreply.github.com>
2022-07-29 13:37:00 -0500

    mcdonald gone woke

---
+++ zcrab/mcdonalds/food/bigmac/pat_wokemcdonalds.lua
@@ -0,0 +1,16 @@
+function init()
+  player.radioMessage({
+    messageId = "pat_wokemcdonalds",
+    unique = false,
+    important = true,
+    text = "Did you just Assume my burger! Im triggered!",
+    senderName = "Woke McDonald's frog",
+    portraitImage = "/zcrab/mcdonalds/food/bigmac/pat_wokemcdonalds.png:<frame>",
+    portraitFrames = 2,
+    portraitSpeed = 0.5,
+    textSpeed = 25,
+    chatterSound = "/sfx/humanoid/frog_chatter"..math.random(1,3)..".ogg"
+  })
+  
+  quest.fail()
+end
