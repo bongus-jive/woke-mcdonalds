e70d456a7bf1915677e3c492693eabaa033b6b9d
patman <27874300+patmanf@users.noreply.github.com>
2025-05-02 21:26:04 -0500

    change the radio message
    i didnt like the way it was written before i think

--- zcrab/mcdonalds/items/food/bigmac/pat_wokemcdonalds.lua
+++ zcrab/mcdonalds/items/food/bigmac/pat_wokemcdonalds.lua
@@ -1,16 +1,13 @@
 function init()
-  player.radioMessage({
-    messageId = "pat_wokemcdonalds",
-    unique = false,
-    important = true,
-    text = "Did you just Assume my burger! Im triggered!",
-    senderName = "Woke McDonald's frog",
-    portraitImage = "/zcrab/mcdonalds/items/food/bigmac/pat_wokemcdonalds.png:<frame>",
-    portraitFrames = 2,
-    portraitSpeed = 0.5,
-    textSpeed = 25,
-    chatterSound = "/sfx/humanoid/frog_chatter"..math.random(1,3)..".ogg"
-  })
-  
   quest.fail()
+
+  local message = config.getParameter("radioMessage")
+  if not message then return end
+
+  local sounds = config.getParameter("chatterSounds")
+  if sounds then
+    message.chatterSound = sounds[math.random(#sounds)]
+  end
+  
+  player.radioMessage(message, delay)
 end
